b1311fef6c2effd8b99f9826cc3e0f84d958b527
Bugs PC <rnaldi491@gmail.com>
2025-04-19 08:40:00 +0900

    types issue again

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,5 +1,5 @@
 
-export const Logo = ({ className }: { className?: string }) => {
+export const Logo = () => {
     return (
         <img alt='logo' src="logo2.png" className='h-16 w-16' />
     )
